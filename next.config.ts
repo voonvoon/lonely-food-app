2f53fc89cca1f11f169b1600ce25e1bfb56fb1e8
voon220 <peter220@gmail.com>
2025-01-07 16:53:32 +0800

    6.item-create-with-image edited2

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['localhost'],
+    // domains: ['localhost'],
+    domains: ['localhost', 'lonely-food-app.vercel.app'],
   },
 };
 
