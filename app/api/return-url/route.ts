746be28e6c391a4dfa28344d1492d5e7dba5ebd0
voon220 <peter220@gmail.com>
2025-02-10 12:12:18 +0800

    10/2-1

--- app/api/return-url/route.ts
+++ app/api/return-url/route.ts
@@ -4,49 +4,19 @@
 //redirect the user to the frontend page after the payment is completed.
 //303 status code indicates that the client should perform a GET request to the new URL instead of original POST.
 
-// import { NextRequest, NextResponse } from "next/server";
-
-// export async function POST(req: NextRequest) {
-//     try {
-//         // Construct the absolute URL for redirection .
-//         const url = new URL(`/order-summary`, req.nextUrl.origin); //req.nextUrl.origin gives you the base URL of the incoming request,
-
-//         // Redirect to the frontend page without passing any data
-//         return NextResponse.redirect(url.toString(), 303); //The 303 makes client to perform a GET request to the new URL.
-//     } catch (error) {
-//         console.error('Error processing request:', error);
-//         return new NextResponse('Internal Server Error', { status: 500 });
-//     }
-// }
-
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
     try {
-        // Read the request body as text
-        const bodyText = await req.text();
-        let body;
+        // Construct the absolute URL for redirection .
+        const url = new URL(`/order-summary`, req.nextUrl.origin); //req.nextUrl.origin gives you the base URL of the incoming request,
 
-        // Try to parse the body text as JSON
-        try {
-            body = JSON.parse(bodyText);
-        } catch (error) {
-            console.error('Invalid JSON:', bodyText);
-            return new NextResponse('Bad Request: Invalid JSON', { status: 400 });
-        }
-
-        console.log('Received parameters backend---------------------------->>:', body);
-
-        // Convert the body to a JSON string and encode it
-        const data = encodeURIComponent(JSON.stringify(body));
-
-        // Construct the absolute URL for redirection with the data parameter
-        const url = new URL(`/order-summary?data=${data}`, req.nextUrl.origin); // req.nextUrl.origin gives you the base URL of the incoming request
-
-        // Redirect to the frontend page with the data as a query parameter
-        return NextResponse.redirect(url.toString(), 303); // The 303 makes client to perform a GET request to the new URL
+        // Redirect to the frontend page without passing any data
+        return NextResponse.redirect(url.toString(), 303); //The 303 makes client to perform a GET request to the new URL.
     } catch (error) {
         console.error('Error processing request:', error);
         return new NextResponse('Internal Server Error', { status: 500 });
     }
 }
+
+
